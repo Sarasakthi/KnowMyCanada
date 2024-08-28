3f5db25353240f8da2e3d81292e59e3aef52b78d
Sarasakthi <saraswathi1404@gmail.com>
2024-08-28 14:31:26 -0600

    Initial commit

---
+++ src/main/java/org/example/knowmycanada/KnowMyCanadaApplication.java
@@ -0,0 +1,13 @@
+package org.example.knowmycanada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowMyCanadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowMyCanadaApplication.class, args);
+    }
+
+}
